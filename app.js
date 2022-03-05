eea450f9841bc80141579a0a72208e69e84f5790
Vivyen Vu <pokeviv@gmail.com>
2022-03-05 18:26:15 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+let data = [
+    {
+        name: "Matero",
+        age: "30"
+    },
+    {
+        name: "Nick",
+        age: "25"
+    },
+    {
+        name: "Dennis",
+        age: "old"
+    },
+    {
+        name: "Roger",
+        age: "27"
+    },
+    {
+        name: "Tai",
+        age: "30"
+    },
+    {
+        name: "Matt",
+        age: "35"
+    },
+    {
+        name: "Clinton",
+        age: "30"
+    },
+]
